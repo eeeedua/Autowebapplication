7ee8022cbfb07209ccd5dd3d3a8c44738d582e8e
Eduardo Ramirez <Eduardo.Ramirez@autoweb.com>
2023-07-21 10:53:17 -0600

    Add project files.

---
+++ Autowebapplication/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Autowebapplication.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
